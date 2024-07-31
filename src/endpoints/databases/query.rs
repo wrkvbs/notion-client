d6259ad4fc22b915cf3788ae453270d137b6897d
Scott Mace <rscottmace@gmail.com>
2024-07-30 21:40:21 -0700

    Debug query a database response body

--- src/endpoints/databases/query.rs
+++ src/endpoints/databases/query.rs
@@ -32,7 +32,7 @@ impl DatabasesEndpoint {
             .text()
             .await
             .map_err(|e| NotionClientError::FailedToText { source: e })?;
-
+        dbg!("Response body: {}", &body);
         let response = serde_json::from_str(&body)
             .map_err(|e| NotionClientError::FailedToDeserialize { source: e })?;
 
